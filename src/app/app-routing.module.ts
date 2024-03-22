fac05ccc6289309135013dc65e3800d713375524
MinettoM <anderson.minetto@softmediaconsultores.com>
2024-03-22 12:50:14 -0500

    9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  }
+  },
+  {
+    path: 'compra-industria',
+    component: CompraIndustriaComponent
+  },
 ];
 
 @NgModule({
